0272efd5f8f98db91ab7f35e6e272b78991d0066
kihyeoon <bangion93@gmail.com>
2023-03-28 17:29:10 +0900

    ➕ 웹팩, 타입스크립트 - 의존성 설치 및 설정파일 생성 #1

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
+const path = require("path");
+
+const isDevelopment = process.env.NODE_ENV !== "production";
+
+module.exports = {
+  mode: process.env.mode,
+  entry: path.join(__dirname, "src/index.tsx"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[hash].js",
+    publicPath: "/",
+    clean: true,
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)?$/,
+        loader: "esbuild-loader",
+        exclude: /node_modules/,
+        options: {
+          loader: "tsx",
+          target: "es2015",
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+    isDevelopment &&
+      new ReactRefreshWebpackPlugin({
+        overlay: false,
+      }),
+  ].filter(Boolean),
+  devServer: {
+    hot: true,
+    open: true,
+  },
+};
