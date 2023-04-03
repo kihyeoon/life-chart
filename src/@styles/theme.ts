37196609bc8c1d2fc9e572bd7de4b9b34da1345d
kihyeoon <bangion93@gmail.com>
2023-04-04 02:02:08 +0900

    💄 theme 설정

---
+++ src/@styles/theme.ts
@@ -0,0 +1,35 @@
+import { COLORS } from "src/@styles/colors";
+import { FONT_SIZE } from "src/@styles/fontSize";
+
+export const LIGHT_MODE_THEME = {
+  COLOR: {
+    PRIMARY: COLORS.BLUE[60],
+    SECONDARY: COLORS.GREY[80],
+    TEXT: {
+      DEFAULT: COLORS.GREY[95],
+      DISABLED: COLORS.GREY[60],
+      PLACEHOLDER: COLORS.GREY[80],
+      LIGHT_BLUE: COLORS.BLUE[50],
+      WHITE: COLORS.WHITE,
+    },
+    BACKGROUND: {
+      DEFAULT: COLORS.GREY[20],
+      SECONDARY: COLORS.WHITE,
+      TERTIARY: COLORS.GREY[50],
+    },
+    CONTAINER: {
+      PRIMARY: COLORS.GREY[20],
+      DEFAULT: COLORS.WHITE,
+    },
+    BORDER: COLORS.GREY[50],
+  },
+  FONT_SIZE: {
+    TITLE: FONT_SIZE["34px"],
+    SUBTITLE: FONT_SIZE["20px"],
+    X_LARGE_BODY: FONT_SIZE["18px"],
+    LARGE_BODY: FONT_SIZE["14px"],
+    BODY: FONT_SIZE["12px"],
+    PLACEHOLDER: FONT_SIZE["12px"],
+    CAPTION: FONT_SIZE["10px"],
+  },
+} as const;
