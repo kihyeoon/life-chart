ae949ee4f30afbe69067d301fd3b092fdd66de0e
kihyeoon <bangion93@gmail.com>
2023-04-08 15:26:32 +0900

    💄 WeekDot 컴포넌트 디자인 수정 #6
    current 상태일때 애니메이션 추가

--- src/components/Chart/WeekDot/WeekDot.style.ts
+++ src/components/Chart/WeekDot/WeekDot.style.ts
@@ -4,34 +4,40 @@ import { StyleType, WeekDotProps } from "./WeekDot";
 
 const styleTable: Record<StyleType, CSSProp<DefaultTheme>> = {
   past: css`
-    background-color: ${({ theme }) => theme.COLOR.PRIMARY.DEFAULT};
+    background-color: ${({ theme }) => theme.COLOR.TEXT.LIGHT_BLUE};
   `,
 
   current: css`
-    background-color: ${({ theme }) => theme.COLOR.PRIMARY.ACTIVE};
-    animation: shine 1.5s infinite;
-    @keyframes shine {
-      0% {
-        opacity: 1;
+    ${({ theme }) => css`
+      background-color: ${theme.COLOR.TEXT.LIGHT_BLUE};
+      animation: shine 1.5s infinite;
+      @keyframes shine {
+        0% {
+          opacity: 1;
+          box-shadow: 0 0 5px ${theme.COLOR.TEXT.LIGHT_BLUE};
+        }
+        50% {
+          transform: scale(1.5);
+          box-shadow: 0 0 10px 5px ${theme.COLOR.TEXT.LIGHT_BLUE},
+            0 0 20px ${theme.COLOR.TEXT.LIGHT_BLUE},
+            0 0 30px ${theme.COLOR.TEXT.LIGHT_BLUE};
+        }
+        100% {
+          opacity: 1;
+          box-shadow: 0 0 5px ${theme.COLOR.TEXT.LIGHT_BLUE};
+        }
       }
-      50% {
-        opacity: 0.5;
-        transform: scale(1.2);
-      }
-      100% {
-        opacity: 1;
-      }
-    }
+    `}
   `,
 
   future: css`
-    background-color: ${({ theme }) => theme.COLOR.TEXT.DISABLED};
+    background-color: ${({ theme }) => theme.COLOR.BACKGROUND.TERTIARY};
   `,
 };
 
 export const Container = styled.li`
-  width: 4px;
-  height: 4px;
+  width: 0.625rem;
+  height: 0.625rem;
   list-style: none;
   border-radius: 100px;
 
