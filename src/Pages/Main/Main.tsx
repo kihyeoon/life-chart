5bd8da90a1c389151a8717531d4896babf18449b
kihyeoon <bangion93@gmail.com>
2023-04-09 17:03:55 +0900

    💄 Main 높이 100vh로 변경 #10

--- src/Pages/Main/Main.tsx
+++ src/Pages/Main/Main.tsx
@@ -2,6 +2,8 @@ import { useLocation } from "react-router-dom";
 
 import Chart from "@src/components/Chart/Chart";
 
+import * as Style from "./Main.style";
+
 interface LocationState {
   state: { birthDay: string; lifeExpectancy: string };
 }
@@ -10,7 +12,11 @@ function Main() {
   const { state } = useLocation() as LocationState;
   const { birthDay, lifeExpectancy } = state;
 
-  return <Chart birthDate={birthDay} years={Number(lifeExpectancy)} />;
+  return (
+    <Style.Container>
+      <Chart birthDate={birthDay} years={Number(lifeExpectancy)} />
+    </Style.Container>
+  );
 }
 
 export default Main;
