2f7fcc4191af08b8fcf2b7ab3ae44b1a00ada548
kihyeoon <bangion93@gmail.com>
2023-04-14 01:11:01 +0900

    ♻️ theme 토글 기능 리팩터링 #13

--- src/hooks/useModeTheme.ts
+++ src/hooks/useModeTheme.ts
@@ -6,12 +6,7 @@ import useWebStorage from "@src/hooks/useWebStorage";
 import { STORAGE_KIND, THEME_KIND } from "@src/@constants";
 import { ThemeKind } from "@src/@types/utils";
 
-interface UseModeThemeResult {
-  theme: ThemeKind;
-  handleToggleTheme: (index: number) => void;
-}
-
-function useModeTheme(): UseModeThemeResult {
+function useModeTheme() {
   const { theme, setTheme } = useThemeContext();
 
   const { getItem: getStoredTheme, setItem: setStoredTheme } =
@@ -25,30 +20,20 @@ function useModeTheme(): UseModeThemeResult {
     setTheme(nextTheme);
   };
 
-  const handleToggleTheme = (index: number) => {
-    const nextTheme = index === 0 ? THEME_KIND.LIGHT : THEME_KIND.DARK;
-    handleChangeTheme(nextTheme);
-  };
-
   useEffect(() => {
     const storedTheme = getStoredTheme();
 
     if (storedTheme === THEME_KIND.DARK || storedTheme === THEME_KIND.LIGHT) {
       handleChangeTheme(storedTheme);
-
       return;
     }
 
-    if (window.matchMedia("(prefers-color-scheme: light)").matches) {
-      handleChangeTheme(THEME_KIND.LIGHT);
-
-      return;
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      handleChangeTheme(THEME_KIND.DARK);
     }
-
-    handleChangeTheme(THEME_KIND.DARK);
   }, []);
 
-  return { theme, handleToggleTheme };
+  return { theme, handleChangeTheme };
 }
 
 export default useModeTheme;
